27dabcc2c41aa1c7fd0b68886014dc0804c379f6
A7Abushaqra <ahmedhossin1122@gmail.com>
2021-01-22 18:33:16 +0200

    shaqra

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fooddeliveryapp/screens/offers_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -12,44 +13,48 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<Widget>_items=[
     Center(child:Container(height: 300,width: 300,color: Colors.deepOrange,)),
-    Center(child:Container(height: 300,width: 300,color: Colors.green,)),
+    OffersScreen(),
     Center(child:Container(height: 300,width: 300,color: Colors.blueAccent,)),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: /*Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [*/
-          CurvedNavigationBar(
-            backgroundColor: Colors.transparent,
-            buttonBackgroundColor: Colors.deepOrange,
-            color: Colors.black,
-            items: <Widget>[
-              Icon(Icons.shopping_cart, size: 30,color: Colors.white,),
-              Icon(Icons.local_offer, size: 30,color: Colors.white,),
-              Icon(Icons.account_circle, size: 30,color: Colors.white,),
-            ],
-            onTap: (index) {
-              setState(() {
-                _CurrentIndex=index;
-              });
-            },
-          ),
-         /* Container(
-            color: Colors.black,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text("Order",style: _CurrentIndex==0?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),),
-                Text("Offers",style:  _CurrentIndex==1?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),),
-                Text("Profile",style:  _CurrentIndex==2?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),)
+      bottomNavigationBar: Container(
+        height: 75,
+        child: Column(
+          children: [
+            CurvedNavigationBar(
+              height: 45,
+              backgroundColor: Colors.transparent,
+              buttonBackgroundColor: Colors.deepOrange,
+              color: Colors.black,
+              items: <Widget>[
+                Icon(Icons.shopping_cart, size: 30,color: Colors.white,),
+                Icon(Icons.local_offer, size: 30,color: Colors.white,),
+                Icon(Icons.account_circle, size: 30,color: Colors.white,),
               ],
+              onTap: (index) {
+                setState(() {
+                  _CurrentIndex=index;
+                });
+              },
             ),
-          ),
-        ],
-      ),*/
+            Container(
+              height: 30,
+              color: Colors.black,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("Order",style: _CurrentIndex==0?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),),
+                  Text("Offers",style:  _CurrentIndex==1?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),),
+                  Text("Profile",style:  _CurrentIndex==2?TextStyle(color: Colors.deepOrange):TextStyle(color: Colors.white),)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _items[_CurrentIndex],
     );
   }
