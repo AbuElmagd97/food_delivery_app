cd7cf8449b2162ca0f43996af17fd22eece98065
A7Abushaqra <ahmedhossin1122@gmail.com>
2021-01-23 17:27:56 +0200

    shaqra

--- lib/screens/Profile_Screen.dart
+++ lib/screens/Profile_Screen.dart
@@ -16,15 +16,18 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Center(
+      body: Container(
+        color: Colors.black87,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: EdgeInsets.all(15),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: MediaQuery.of(context).size.height*0.05,),
-              Text("Discover Places",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 30),),
-              Text("Around you",style: TextStyle(color: Colors.black,fontSize: 20),),
+              Text("Discover Places",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 30),),
+              Text("Around you",style: TextStyle(color: Colors.white,fontSize: 20),),
               Padding(
                 padding: EdgeInsets.only(bottom: 15,left: 30,top:30),
                 child: SignInButton(
@@ -44,7 +47,7 @@ class ProfileScreen extends StatelessWidget {
                     }),
               ),
               SizedBox(height: MediaQuery.of(context).size.height*0.3,),
-              Text("    Have Something Thriii Feh?"),
+              Text("    Have Something Thriii Feh?",style: TextStyle(color: Colors.white70),),
               Row(
                children: [
                  SocialMediaButton.whatsapp(
