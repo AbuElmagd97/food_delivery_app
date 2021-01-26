71174f60ce6d9b7e2897402e38aaf747972a355f
Mohamed.AbuElmagd <mohamed.aboelmagd2012@gmail.com>
2021-01-26 06:42:41 +0200

    Completed order_screen.dart UI Added 2 Widgets fb_reaction.dart & social_box.dart

--- lib/screens/Settings_Screen.dart
+++ lib/screens/Settings_Screen.dart
@@ -1,102 +1,125 @@
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatefulWidget {
-  static const RouteName="/SettingScreen";
+  static const RouteName = "/SettingScreen";
 
   @override
   _SettingScreenState createState() => _SettingScreenState();
 }
 
 class _SettingScreenState extends State<SettingScreen> {
-  bool LanguageIsEnglish=true;
+  bool languageIsEnglish = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-       backgroundColor: Colors.deepOrange,
-       leading: IconButton(
-         icon: Icon(Icons.arrow_back,size: 35,color: Colors.white),
-         onPressed: (){
-           Navigator.of(context).pop();
-           },
-       ),
-       title: Text("Settings",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
-     ),
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, size: 35, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text(
+          "Settings",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
       body: Container(
         color: Colors.black87,
-        child:Column(
-            children: [
-                Padding(
-                  padding: const EdgeInsets.all(12),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Language",
+                    style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  Row(
                     children: [
-                      Text("Language",style: TextStyle(fontSize: 17,fontWeight:FontWeight.bold,color: Colors.white),),
-                      Row(
-                        children: [
-                          LanguageIsEnglish?Container(
-                            height: 40,
-                            color: Colors.black87,
-                            child:  RaisedButton(
+                      languageIsEnglish
+                          ? Container(
+                              height: 40,
                               color: Colors.black87,
-                              onPressed: (){
-                                setState(() {
-                                  LanguageIsEnglish=false;
-                                  print(LanguageIsEnglish);
-                                });
-                              },
-                              child: Text("عربى",style: TextStyle(color: Colors.white)),
-                            ),
-                          )
-                          :Container(
-                            height: 40,
-                            color:Colors.deepOrange,
-                            child: FlatButton(
-                            onPressed: (){
-                            },
-                            child: Text("عربى",style: TextStyle(color: Colors.white)),
-                          ),),
-                          LanguageIsEnglish? Container(
-                            height: 40,
-                            color: Colors.deepOrange,
-                            child: FlatButton(
-                              onPressed: (){
-                              },
-                              child: Text("English",style: TextStyle(color: Colors.white)),
-                            ),
-                          )
+                              child: RaisedButton(
+                                color: Colors.black87,
+                                onPressed: () {
+                                  setState(() {
+                                    languageIsEnglish = false;
+                                    print(languageIsEnglish);
+                                  });
+                                },
+                                child: Text("عربى",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )
                           : Container(
-                                height: 40,
-                                color:Colors.black87,
-                                child: RaisedButton(
-                                  color: Colors.black87,
-                                         onPressed: (){
-                                               setState(() {
-                                                  LanguageIsEnglish=true;
-                                                  print(LanguageIsEnglish);
-                                               });
-                           },
-                                 child: Text("English",style: TextStyle(color: Colors.white)),
-                              ),),
-                        ],
-                      ),
+                              height: 40,
+                              color: Colors.deepOrange,
+                              child: FlatButton(
+                                onPressed: () {},
+                                child: Text("عربى",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ),
+                      languageIsEnglish
+                          ? Container(
+                              height: 40,
+                              color: Colors.deepOrange,
+                              child: FlatButton(
+                                onPressed: () {},
+                                child: Text("English",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            )
+                          : Container(
+                              height: 40,
+                              color: Colors.black87,
+                              child: RaisedButton(
+                                color: Colors.black87,
+                                onPressed: () {
+                                  setState(() {
+                                    languageIsEnglish = true;
+                                    print(languageIsEnglish);
+                                  });
+                                },
+                                child: Text("English",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                            ),
                     ],
                   ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.exit_to_app,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
                 ),
-                Divider(color: Colors.grey,),
-                Row(
-                  children: [
-                    IconButton(
-                        icon:Icon(Icons.exit_to_app,color: Colors.white,),
-                       onPressed: (){},
-                    ),
-                    FlatButton(child: Text("Log Out",style: TextStyle(color: Colors.white)),onPressed: (){}, )
-                  ],
-                ),
-            ],
-          ),
+                FlatButton(
+                  child: Text("Log Out", style: TextStyle(color: Colors.white)),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -9,7 +9,6 @@ class OrderScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    var height = MediaQuery.of(context).size.height;
     return SafeArea(
       child: SingleChildScrollView(
         scrollDirection: Axis.vertical,
