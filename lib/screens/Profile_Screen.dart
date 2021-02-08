45e8cb44575fa1fc69fe4c9cda4ce85f56158392
abushaqra <ahmedhossin1122@gmail.com>
2021-02-08 16:20:20 +0200

    google sign in bs lesa fadel elpage tt3'er

--- lib/screens/Profile_Screen.dart
+++ lib/screens/Profile_Screen.dart
@@ -1,81 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryapp/services/auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_button/sign_button.dart';
 import 'package:social_media_buttons/social_media_buttons.dart';
 
 class ProfileScreen extends StatefulWidget {
-  static bool LoginingIn=false;
-
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  GoogleSignInAccount _currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ProfileScreen.LoginingIn?LoginingIN():LoginingOut(),
-    );
-  }
-}
-
-class LoginingOut extends StatefulWidget {
-  const LoginingOut({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  _LoginingOutState createState() => _LoginingOutState();
-}
-
-class _LoginingOutState extends State<LoginingOut> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black87,
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Padding(
-        padding: EdgeInsets.all(15),
+      body: _currentUser!=null?Container(
+        color: Colors.black87,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            ListTile(
+              leading: GoogleUserCircleAvatar(
+              placeholderPhotoUrl: _currentUser.photoUrl,
+              ),
+              title: Text(_currentUser.displayName ?? '',
+                style: TextStyle(color: Colors.white),),
+              subtitle: Text(_currentUser.email ?? '',
+                style: TextStyle(color: Colors.white),),
+            ),
+            /*Row(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.transparent,
+                  child: Icon(Icons.account_circle_outlined,size: 80,color: Colors.white,),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("User Name",
+                        style: TextStyle(color: Colors.white),),
+                      Text("Email@Example.com",
+                        style: TextStyle(color: Colors.white),)
+                    ],
+                  ),
+                ),
+              ],
+            ),*/
+            Divider(color: Colors.grey,),
+            ListTile(
+              leading: Icon(Icons.shopping_cart_outlined,color: Colors.deepOrange,),
+              title: Text("My Order",
+                style: TextStyle(color: Colors.white),),
+              trailing: Icon(Icons.arrow_forward_ios,color: Colors.deepOrange,),
+            ),
+            Divider(color: Colors.grey,),
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05,
-            ),
-            Text(
-              "Discover Places",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30),
-            ),
-            Text(
-              "Around you",
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 15, left: 30, top: 30),
-              child: SignInButton(
-                  buttonType: ButtonType.google,
-                  buttonSize: ButtonSize.large,
-                  onPressed: () {
-                    setState(() {
-                     ProfileScreen.LoginingIn=true;
-                     print( ProfileScreen.LoginingIn);
-                    });
-                  }),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 15, left: 30),
-              child: SignInButton(
-                  buttonType: ButtonType.facebook,
-                  buttonSize: ButtonSize.large,
-                  onPressed: () {
-                    print('click');
-                  }),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.25,
+              height: MediaQuery.of(context).size.height * 0.35,
             ),
             Text(
               "    Have Something Thriii Feh?",
@@ -98,78 +84,82 @@ class _LoginingOutState extends State<LoginingOut> {
             )
           ],
         ),
-      ),
-    );
-  }
-}
+      )
 
-class LoginingIN extends StatelessWidget {
-  const LoginingIN({
-    Key key,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black87,
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
+
+
+
+          :Container(
+        color: Colors.black87,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.transparent,
-                child: Icon(Icons.account_circle_outlined,size: 80,color: Colors.white,),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("User Name",
-                      style: TextStyle(color: Colors.white),),
-                    Text("Email@Example.com",
-                      style: TextStyle(color: Colors.white),)
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Divider(color: Colors.grey,),
-          ListTile(
-            leading: Icon(Icons.shopping_cart_outlined,color: Colors.deepOrange,),
-            title: Text("My Order",
-              style: TextStyle(color: Colors.white),),
-            trailing: Icon(Icons.arrow_forward_ios,color: Colors.deepOrange,),
-          ),
-          Divider(color: Colors.grey,),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.35,
-          ),
-          Text(
-            "    Have Something Thriii Feh?",
-            style: TextStyle(color: Colors.white70),
-          ),
-          Row(
-            children: [
-              SocialMediaButton.whatsapp(
-                onTap: () {
-                  print('or just use onTap callback');
-                },
-                size: 35,
-                color: Colors.green,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
               ),
               Text(
-                " Chat with us on whatsapp",
-                style: TextStyle(color: Colors.green),
+                "Discover Places",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+              Text(
+                "Around you",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 15, left: 30, top: 30),
+                child: SignInButton(
+                    buttonType: ButtonType.google,
+                    buttonSize: ButtonSize.large,
+                    onPressed: () async{
+                      await signInwithGoogle();
+                      print(_currentUser);
+                    }),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 15, left: 30),
+                child: SignInButton(
+                    buttonType: ButtonType.facebook,
+                    buttonSize: ButtonSize.large,
+                    onPressed: () async{
+                      await signOut();
+                      print(_currentUser);
+                    }),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.25,
+              ),
+              Text(
+                "    Have Something Thriii Feh?",
+                style: TextStyle(color: Colors.white70),
+              ),
+              Row(
+                children: [
+                  SocialMediaButton.whatsapp(
+                    onTap: () {
+                      print('or just use onTap callback');
+                    },
+                    size: 35,
+                    color: Colors.green,
+                  ),
+                  Text(
+                    " Chat with us on whatsapp",
+                    style: TextStyle(color: Colors.green),
+                  )
+                ],
               )
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
 }
+
---
+++ lib/services/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+Future<void> signInwithGoogle()async {
+  await Firebase.initializeApp();
+  final GoogleSignInAccount googleSignInAccount =
+  await _googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+  await googleSignInAccount.authentication;
+  final AuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleSignInAuthentication.accessToken,
+    idToken: googleSignInAuthentication.idToken,
+  );
+ UserCredential userCredential=await _auth.signInWithCredential(credential);
+ User user = userCredential.user;
+ print(user.email);
+}
+Future<void> signOut()async{
+  await _googleSignIn.signOut();
+}
